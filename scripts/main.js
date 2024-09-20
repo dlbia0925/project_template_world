9211f4aa30150d00b80d3c6cdf0c29439e45f139
hmc <hmc2020925@gmail.com>
2024-09-20 00:30:57 -0600

    finish styling navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,25 @@
-$(function () {
-    var b = $("#navtitle");
-    var w = $("#wrapper");
-    var l = $("#list");
+$(document).ready(function () {
+    // Loop through each navtitle
+    $(".nav-second-title").each(function() {
+        var b = $(this); // Current navtitle
+        var w = b.next(".wrapper"); // Next sibling wrapper
 
-    w.height(l.outerHeight(true));
-
-    b.click(function () {
-
-        if (w.hasClass('open')) {
-            w.removeClass('open');
-            w.height(0);
+        // Set initial height based on whether 'open' class exists
+        if (w.hasClass("open")) {
+            w.height(w.find("ul").outerHeight(true));
         } else {
-            w.addClass('open');
-            w.height(l.outerHeight(true));
+            w.height(0); // Closed by default
         }
 
+        // Click event to toggle the wrapper
+        b.click(function () {
+            if (w.hasClass('open')) {
+                w.removeClass('open');
+                w.height(0); // Collapse
+            } else {
+                w.addClass('open');
+                w.height(w.find("ul").outerHeight(true)); // Expand
+            }
+        });
     });
 });
